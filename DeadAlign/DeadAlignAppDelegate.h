d2ea3939468f7892235d6a81974ef54d71b84e7a
Larry Cao <larry.cao@gmail.com>
2012-04-30 20:37:33 -0700

    Initial Commit

---
+++ DeadAlign/DeadAlignAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DeadAlignAppDelegate.h
+//  DeadAlign
+//
+//  Created by Admin on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DeadAlignAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ DeadAlign/DeadAlignAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  DeadAlignAppDelegate.m
+//  DeadAlign
+//
+//  Created by Admin on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DeadAlignAppDelegate.h"
+
+@implementation DeadAlignAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
